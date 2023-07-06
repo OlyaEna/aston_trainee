140690265a912b1264a3067dac14a6c246516352
olyao <olya.olka.ena@mail.ru>
2023-07-06 14:05:26 +0300

    init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,41 +1,6 @@
 package org.example;
 
-import org.example.hw1_customArrayList.CustomArrayList;
-import org.example.hw1_customArrayList.CustomList;
-
-
-
 public class Main {
     public static void main(String[] args) {
-        CustomList<String > strings = new CustomArrayList<>(4);
-        strings.add("Один");
-        strings.add("Два");
-        strings.add("Четыре");
-        strings.add("Три");
-        System.out.println(strings);
-
-
-        strings.sort();
-        for (int i = 0; i < strings.size(); i++) {
-            System.out.print(strings.get(i) + " ");
-        }
-        System.out.println(strings);
-
-
-
-//        strings1.add("Один");
-//        strings1.add("Два");
-//        strings1.add("Три");
-//        strings1.add("Три");
-//
-//
-//        System.out.println(strings1);
-
-
-
-
-
-
-
     }
 }
